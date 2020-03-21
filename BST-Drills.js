46b1810cfb2befc9a13764eb00f9cf7360fb18f6
Anthony Clark Perfecto <a1clark1a@gmail.com>
2020-03-21 00:56:44 -0700

    Heightoftree and isTreeBST drills

--- BST-Drills.js
+++ BST-Drills.js
@@ -8,12 +8,12 @@ function main() {
         BST.insert(n);
     })
     
+    console.log(heightOfTree(BST))
     
+    //console.log(BST)
     
-    console.log(BST)
-    
-    BST.remove(3);
-    console.log(BST)
+    //BST.remove(3);
+    //console.log(BST)
     
     
     const BST2 = new BinarySearchTree;
@@ -24,12 +24,42 @@ function main() {
    })
     
    // console.log(BST2)
+    console.log(isTreeBST(BST)) //true
     
 }
 
-function heighOfTree(tree) {
+function heightOfTree(tree) {
+    if(!tree){
+        return 0;
+    }
+    if(!tree.left && !tree.right){
+        return 1;
+    }
+    let height = 0;
+    if(tree.right){
+        let rightHeight = 1 + heightOfTree(tree.right);
+        if(rightHeight > height) {
+            height = rightHeight;
+        }
+    }
+    if(tree.left){
+        let leftHeight = 1 + heightOfTree(tree.left);
+        if(leftHeight > height) {
+            height = leftHeight
+        }
+    }
+    return height;
     
-    
+}
+
+function isTreeBST(tree){
+    if(!tree){
+        return 'empty'
+    }
+    if(tree.left.key > tree.key){
+        return true;
+    }
+    return false;
 }
 
 main();
